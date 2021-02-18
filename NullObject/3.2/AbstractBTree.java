0886bdc8c584fee0628df465f0365050257459f6
Danisovszky Márk <dmarkprog@gmail.com>
2021-02-18 14:45:12 +0100

    clean

--- NullObject/3.2/AbstractBTree.java
+++
@@ -1,9 +0,0 @@
-public abstract class AbstractBTree {
-    public abstract String toStringPreOrder();
-
-    public abstract String toStringPostOrder();
-
-    public abstract String toStringInOrder();
-
-    public abstract int sum();
-}
--- TemplateMethod/5.2/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        TemplateMethod tm1 = new Tee();
-        String s = tm1.brewBeverage();
-        System.out.print(s);
-    }
-}
